c4f6a48ad9c7febda2e5a925f873221a90576935
@whitwo <jwhitwo@gmail.com>
2015-12-30 11:10:20 -0500

    First commit

---
+++ HelloGit.java
@@ -0,0 +1,18 @@
+package hellogit;
+
+/**
+ * Author: Jeff Whitworth
+ * Program 0 - HelloGit
+ * CSC230-02 Spring 2016
+ */
+public class HelloGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //outputs the string "Hello World!" to the console
+        System.out.println("Hello World!");
+    }
+    
+}
